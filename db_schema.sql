f47d8840fa17934b289200d32ec69c5111020d15
MariuszLyszczarzAigo <mariusz.lyszczarz@aigorithmics.com>
2023-09-15 23:01:47 +0200

    fix some issues

--- db_schema.sql
+++ db_schema.sql
@@ -1,18 +1,16 @@
-CREATE TABLE user (
-    "id" UUID,
+CREATE TABLE "user" (
+    "id" UUID PRIMARY KEY,
     "first_name" VARCHAR(255),
     "last_name" VARCHAR(255),
     "phone_number" VARCHAR(15),
     "address" VARCHAR(255),
-    "login" VARCHAR(255),
-    "password" VARCHAR(255),
-    CONSTRAINT user_pk PRIMARY KEY (id, login)
+    "login" VARCHAR(255) UNIQUE,
+    "password" VARCHAR(255)
 );
 
-CREATE TABLE balance (
+CREATE TABLE "balance" (
     "id" UUID PRIMARY KEY,
     "balance" FLOAT,
     "currency" VARCHAR(15),
-    CONSTRAINT balance_pk PRIMARY KEY (id),
-    CONSTRAINT balance_fkey FOREIGN KEY ("id") REFERENCES user("id")
+    CONSTRAINT balance_fkey FOREIGN KEY ("id") REFERENCES "user"("id")
 );
